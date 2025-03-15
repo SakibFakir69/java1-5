8407ffcd5de03b0046d665d9e2dc566e942e487d
Sakib Fakir <FakirSakib22232@gmail.com>
2025-03-15 20:50:56 +0600

    copy conscuter

--- sakib.java
+++ sakib.java
@@ -4,25 +4,57 @@
 
 // java mid term 1-5 
 
+/// chapter 1 
+// class Area{
 
-class Area{
-
-    void show(){
+//     void show(){
         
-        int h =10;
-        int w =20;
-        int ans = h*w;
-        System.out.println("Answer is: "+ans);
+//         int h =10;
+//         int w =20;
+//         int ans = h*w;
+//         System.out.println("Answer is: "+ans);
+//     }
+// }
+
+class Test{
+
+    int roll;
+    int age ;
+
+    Test(int roll, int age){
+        this.roll=roll;
+        this.age=age;
+    }
+
+    Test(Test s)
+    {
+        this.roll=s.age;
+        this.age=s.age;
+    }
+
+    void Show(){
+        System.out.println("call");
+        
+        System.out.println("Your age is  "+roll);
+        System.out.println("Your roll is a  : "+age);
     }
 }
 
 
 public class sakib {
 
-    public static void main(String[] arggs){
+    public static void main(String[] args){
 
-        Area x = new Area();
-        x.show();
+
+        Test x = new Test(10, 12);
+   
+        Test x2 = new Test(x);
+        x2.Show();
+        x.Show();
+
+
+
+      
 
        
 
