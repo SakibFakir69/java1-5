4e96c040b580e04d911a83f38a783e5a23c144e7
Sakib Fakir <FakirSakib22232@gmail.com>
2025-03-15 20:37:41 +0600

    area problem solved

--- sakib.java
+++ sakib.java
@@ -4,12 +4,27 @@
 
 // java mid term 1-5 
 
+
+class Area{
+
+    void show(){
+        
+        int h =10;
+        int w =20;
+        int ans = h*w;
+        System.out.println("Answer is: "+ans);
+    }
+}
+
+
 public class sakib {
 
     public static void main(String[] arggs){
 
-        System.out.println("mid term 3/16/25");
+        Area x = new Area();
+        x.show();
 
+       
 
 
         
