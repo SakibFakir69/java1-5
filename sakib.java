345622280d6e4c18cf5367c2c6af57a8e2db2781
Sakib Fakir <FakirSakib22232@gmail.com>
2025-03-15 20:32:18 +0600

    ok

--- sakib.java
+++ sakib.java
@@ -1,3 +1,20 @@
 
 
 
+
+// java mid term 1-5 
+
+public class sakib {
+
+    public static void main(String[] arggs){
+
+        System.out.println("mid term 3/16/25");
+
+
+
+        
+    
+    }
+
+    
+}
